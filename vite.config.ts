16a90202c86565f00ecc272177f727b6672930c5
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2025-12-23 22:41:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
-  base: '//',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
