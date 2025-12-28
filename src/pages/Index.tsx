bcce1702a0605d3a0f384be5d1a3bd4c9529e28e
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2025-12-28 14:06:21 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import { Badge } from "@/components/ui/badge";
 import { ShoppingCart } from "lucide-react";
 import { useState } from "react";
 import Navigation from "@/components/Navigation";
-import Footer from "@/components/Footer";
+//import Footer from "@/components/Footer";
 import classicStories from './images/classic-malayalam-stories.jpg';
 import timepiece from './images/the-story-of-the-timepiece.jpg';
 import gandhiAlive from './images/gandhi-alive.jpg';
@@ -455,7 +455,7 @@ const Index = () => {
         </div>
       </section>
 
-      <Footer />
+      {/*<Footer />*/}
     </div>
   );
 };
